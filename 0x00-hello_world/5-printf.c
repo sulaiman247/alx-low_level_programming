72a4bfc2e2ff6c5f1edf59906da64869e92288b9
sulaiman247 <pablourban247@gmail.com>
2022-09-11 13:59:06 -0700

    this is using printf to print a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is going to print a particular text to the screen.
+ * Return: this going to return 0
+ */
+int main(void)
+{
+	printf("%s\n","with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
