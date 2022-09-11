5c6ff5bbb69b67221a0013ee5320ca8f56a0d280
sulaiman247 <pablourban247@gmail.com>
2022-09-11 13:16:21 -0700

    corrected the code to meet betty standard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main() - display a text programming is like building a multilingual puzzle. 
- */
+*main() - display a text programming is like building a multilingual puzzle.
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
