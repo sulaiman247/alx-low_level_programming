5d6d99bc2296d5e188c839f67c51a04dbea338bb
sulaiman247 <pablourban247@gmail.com>
2022-09-21 16:30:35 -0700

    number 0-9 in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this will print all numbers in base 16
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 30; i <= 39; i++)
+	{
+		putchar(i);
+	}
+	for (c = 'a'; c <= f; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
