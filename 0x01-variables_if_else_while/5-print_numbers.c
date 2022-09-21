23bfef57643e8fb289c4324ebefe5978ba9cb815
sulaiman247 <pablourban247@gmail.com>
2022-09-21 14:33:20 -0700

    all single digit unsigned numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - how to print numbers of range 0 - 9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
