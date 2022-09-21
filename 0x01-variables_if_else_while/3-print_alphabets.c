5d4bfddf6be8bc76adf7040ba8975b85da471ace
sulaiman247 <pablourban247@gmail.com>
2022-09-21 14:04:07 -0700

    how to generate alphabets in upper and lowercase together

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function that prints the english alphabet from a-z in up
+ * uppper and lower case.
+ *Return: 0
+ */
+int main(void)
+{
+	char b;
+	char c;
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		putchar(b);
+	}
+	for  (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
