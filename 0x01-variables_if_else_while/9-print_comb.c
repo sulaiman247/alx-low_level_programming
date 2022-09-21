da8997a84a7f1327fa9f9c2391f872719150afcb
sulaiman247 <pablourban247@gmail.com>
2022-09-21 16:49:43 -0700

    all numbers seperated by space and comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function that pront all combination of a single digit
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if ( i < 57)
+		{
+			putchar(44);
+			putchar(' ');
+		}
+	}
+	return (0);
+}
+
+
