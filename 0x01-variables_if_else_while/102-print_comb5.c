99f40433e8b8c38211bf144e95f1d471b55918c2
sulaiman247 <pablourban247@gmail.com>
2022-09-21 18:43:09 -0700

    new one

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = 0; b < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i/10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
