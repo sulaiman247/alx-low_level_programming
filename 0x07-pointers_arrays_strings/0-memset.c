286dde6305236a861f602f36e542a1a0e9fce6c6
sulaiman247 <pablourban247@gmail.com>
2022-10-06 09:21:36 -0700

    correcting some betty style errors

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,6 @@
 
-/*memset -  this is eill replace the me
+/*
+ * _memset -  this is eill replace the me
  *
  *@*s : array to be passed
  *@b : character to replace memory
@@ -7,13 +8,13 @@
 *
 * Return: this will return a pointer.
 *
- */
-
-
+*/
 
 char *_memset(char *s, char b, unsigned int n)
-{	
-	unsigned int i = 0 ;
+{
+	unsigned int i; 
+	i = 0 ;
+
 	while (i < n)
 	{
 		*(s + i) = b;
