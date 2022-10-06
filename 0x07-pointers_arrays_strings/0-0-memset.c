44e7667a09117154c820dac0ac7123c04df8a4e8
sulaiman247 <pablourban247@gmail.com>
2022-10-06 05:32:05 -0700

    more on functions, here i have created a memset function

---
+++ 0x07-pointers_arrays_strings/0-0-memset.c
@@ -0,0 +1,24 @@
+#include <main.h>>
+
+/*memset -  this is eill replace the me
+ *
+ *@*s : array to be passed
+ *@b : character to replace memory
+*@n : number of memory space to replace
+*
+* Return: this will return a pointer.
+*
+ */
+
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0 ;
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
+}
