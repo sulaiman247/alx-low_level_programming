2b7853f01bacdc221c854f5a050d23813b8e3791
sulaiman247 <pablourban247@gmail.com>
2022-10-09 07:23:16 -0700

    prototype in main wasnt correct

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 /**
  * _puts_recursion(char *s) - this is a function that print a string output using recursion.
@@ -10,8 +11,10 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
+	_putchar('\n');
+
 	_putchar(*s);
+
 	_puts_recursion(s + 1);
 }
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,2 +1,2 @@
-_putchar
+int _putchar(char c)
 void _puts_recursion(char *s)
