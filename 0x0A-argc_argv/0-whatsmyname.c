1c821a4a68370f36fdfb2fbc7a36539aabdc1898
sulaiman247 <pablourban247@gmail.com>
2022-10-09 18:06:26 -0700

    this is a main.h and a 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+/**
+ * main - print the first arguments
+ *  passed to commandline.
+ *
+ * @argc: counts the number of arguments passed
+ * @argv: store the array of strings
+ *
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+working on commandlinr input project
