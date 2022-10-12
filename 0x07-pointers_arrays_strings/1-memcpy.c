3b8719364a0d7a7d5b1ba850dbc1ea7b50657fdb
sulaiman247 <pablourban247@gmail.com>
2022-10-12 03:52:57 -0700

    added a new function that copies data from a pointer and store it in another pointer

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,12 +10,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 	i = 0;
 
 	while(i < n)
 	{
-		dest+i = src+i;
+		dest+i = *(src+i);
 			i++;
 	}
 	return (dest);
