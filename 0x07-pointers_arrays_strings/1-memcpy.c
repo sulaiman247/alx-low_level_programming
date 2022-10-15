71c7ec7e9daa9ea43fa37a94d27f1854902a031f
sulaiman247 <pablourban247@gmail.com>
2022-10-15 07:36:29 -0700

    iteraring a pointer

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,10 +13,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 	i = 0;
 
-	while( i < n )
+	while (i < n)
 	{
 		dest[i] = src[i];
 			i++;
 	}
-	return ( dest );
+	return (dest);
 }
